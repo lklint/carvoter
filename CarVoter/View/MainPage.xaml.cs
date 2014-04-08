372428f2e8c673a4f7df4f4ad65f6e6572487f4c
lklint <lars@klint.co>
2014-04-08 23:38:43 +1000

    Initial Commit

---
+++ CarVoter/View/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CarVoter
+{
+    public partial class MainPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
