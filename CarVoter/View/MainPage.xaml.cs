d0f721eff4ceaf61a612062b431d8dabb6d1d419
lklint <lars@klint.co>
2014-04-08 23:42:09 +1000

    Added loaded event.

--- CarVoter/View/MainPage.xaml.cs
+++ CarVoter/View/MainPage.xaml.cs
@@ -6,6 +6,13 @@
         public MainPage()
         {
             InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
